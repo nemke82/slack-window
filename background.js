73aaf102d0f2682373c6f0490b2e5ef8973308b6
Nemanja Djuric <ne@nemanja.io>
2021-04-16 19:12:48 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,29 @@
+/*
+ * Window popup using page action
+ *
+ * @ref: https://developer.chrome.com/extensions/pageAction
+ * @ref: https://developer.chrome.com/extensions/examples/api/pageAction/set_icon.zip
+ * This is a workaround to make sure chrome.pageAction.onClicked works.
+ *
+ * The idea here to to force popup to show on GitHub and GitLab pages,
+ * but as we don't assign popup, it fails and called onClicked.
+ * If we don't do this, onClicked won't be triggered.
+ */
+
+const forcePopup = ({ id, url }) => {
+  chrome.pageAction.show(id);
+};
+
+chrome.tabs.onUpdated.addListener((tabId, activeInfo, tab) => {
+  forcePopup(tab);
+});
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  chrome.tabs.get(tabId, forcePopup);
+});
+
+chrome.pageAction.onClicked.addListener(({ url }) => {
+  let popupUrl = 'https://magemojo.slack.com/';
+
+  chrome.windows.create({ url: popupUrl, type: 'popup' });
+});
